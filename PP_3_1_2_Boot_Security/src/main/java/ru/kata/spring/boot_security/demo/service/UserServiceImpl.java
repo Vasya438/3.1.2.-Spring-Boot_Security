4a8ce783718f58a71bd31d92cc9cefb824b8c849
Vasya438 <fric_vasya@mail.ru>
2024-06-14 17:03:56 +0300

    2

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/service/UserServiceImpl.java
@@ -14,7 +14,6 @@ import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
-
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
 
@@ -25,13 +24,11 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
-    @Transactional
     public List<User> getAllUsersList() {
         return userRepository.findAll();
     }
 
     @Override
-    @Transactional
     public User getUserById(Long id) {
         return userRepository.findById(id).orElse(null);
     }
